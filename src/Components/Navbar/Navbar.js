4ef6cc2661990e1d2280e115d107dd530583cb9c
Mahathir-Gunjon <mahathirgunjon@gmail.com>
2022-03-26 00:55:09 +0600

    navbar and projects ready

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <div className="container">
+                    <p className="navbar-brand" href="#">Navbar</p>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <i className="nav-link active" aria-current="page" href="#">Shop</i>
+                            </li>
+                            <li className="nav-item">
+                                <i className="nav-link" href="#">Pricing</i>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
